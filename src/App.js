ed00df5fd9cb27021ed8d9e6f74ebae025ababf2
Shelby Christensen <shelbchrist@yahoo.com>
2025-08-04 21:48:50 -0500

    Setup state variables for advice, loading and error

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [advice, setAdvice] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  
   return (
     <div className="App">
       <h1>Ask the Force</h1>
