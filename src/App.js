2aa460103ff3e13bf28a295711afb1632ce6efcd
Shelby Christensen <shelbchrist@yahoo.com>
2025-08-04 21:22:12 -0500

    Setup react app and simplify Appjs file, css file and test it in the browser

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ask the Force</h1>
+      <p>Receive advice from beyond.</p>
+    </div>
+  );
+}
+
+export default App;
